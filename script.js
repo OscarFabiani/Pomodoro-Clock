66dec126b6bd5c296fb9369458f0cc64371c5b4c
OscarFabiani <spartan539@yahoo.com>
2019-03-06 15:59:24 -0800

    Refactored functions, added swap function, and added audio.

--- script.js
+++ script.js
@@ -1,41 +1,90 @@
-const breakLength = document.getElementById('break-length');
-let breakLengthVal = Number(breakLength.textContent);
 
+//SETTING VARIABLES
 const breakDecrement = document.getElementById('break-decrement');
 const breakIncrement = document.getElementById('break-increment');
+const breakLength = document.getElementById('break-length');
+const sessionDecrement = document.getElementById('session-decrement');
+const sessionIncrement = document.getElementById('session-increment');
+const sessionLength = document.getElementById('session-length');
+const timerLabel = document.getElementById('timer-label');
+const timeLeft = document.getElementById('time-left');
+const startStop = document.getElementById('start_stop');
+const reset = document.getElementById('reset');
+const beep = document.getElementById('beep');
 
+
+let breakLengthVal = Number(breakLength.textContent);
+let sessionLengthVal = Number(sessionLength.textContent);
+
+let minsLeft = Number(timeLeft.textContent.match(/\d+/));
+let secsLeft = Number(timeLeft.textContent.match(/(?<=:)\d+/));
+
+
+//ADDING EVENT LISTENERS
+breakDecrement.addEventListener('click', subtractBreak);
+breakIncrement.addEventListener('click', addBreak);
+sessionDecrement.addEventListener('click', subtractSession);
+sessionIncrement.addEventListener('click', addSession);
+startStop.addEventListener('click', toggleTimer);
+reset.addEventListener('click', resetTimer);
+
+
+let intervalID;
+let paused = true;
+
+resetTimer();
+
+
+//FUNCTIONS
 
 function subtractBreak() {
-  if (breakLengthVal > 1) {
+  if (paused == true && breakLengthVal > 1) {
     breakLengthVal--;
     breakLength.textContent = breakLengthVal;
+    if (timerLabel.textContent == 'Break') {
+      minsLeft = breakLengthVal;
+      secsLeft = 0;
+      if (minsLeft < 10) {
+        timeLeft.textContent = '0' + minsLeft + ':00';
+      }
+      else {
+        timeLeft.textContent = minsLeft + ':00';
+      }
+    }
   }
 }
 
 function addBreak() {
-  if (breakLengthVal < 60) {
+  if (paused == true && breakLengthVal < 60) {
     breakLengthVal++;
     breakLength.textContent = breakLengthVal;
+    if (timerLabel.textContent == 'Break') {
+      minsLeft = breakLengthVal;
+      secsLeft = 0;
+      if (minsLeft < 10) {
+        timeLeft.textContent = '0' + minsLeft + ':00';
+      }
+      else {
+        timeLeft.textContent = minsLeft + ':00';
+      }
+    }
   }
 }
-
-breakDecrement.addEventListener('click', subtractBreak);
-breakIncrement.addEventListener('click', addBreak);
-
-const sessionLength = document.getElementById('session-length');
-let sessionLengthVal = Number(sessionLength.textContent);
-
-const sessionDecrement = document.getElementById('session-decrement');
-const sessionIncrement = document.getElementById('session-increment');
 
 function subtractSession() {
   if (paused == true && sessionLengthVal > 1) {
     sessionLengthVal--;
     sessionLength.textContent = sessionLengthVal;
-    minsLeft = sessionLengthVal;
-    secsLeft = 0;
-    timeLeft.textContent = minsLeft + ':00';
-
+    if (timerLabel.textContent == 'Session') {
+      minsLeft = sessionLengthVal;
+      secsLeft = 0;
+      if (minsLeft < 10) {
+        timeLeft.textContent = '0' + minsLeft + ':00';
+      }
+      else {
+        timeLeft.textContent = minsLeft + ':00';
+      }   
+    }
   }
 }
 
@@ -43,79 +92,106 @@ function addSession() {
   if (paused == true && sessionLengthVal < 60) {
     sessionLengthVal++;
     sessionLength.textContent = sessionLengthVal;
-    minsLeft = sessionLengthVal;
-    secsLeft = 0;
-    timeLeft.textContent = minsLeft + ':00';
+    if (timerLabel.textContent == 'Session') {
+      minsLeft = sessionLengthVal;
+      secsLeft = 0;
+      if (minsLeft < 10) {
+        timeLeft.textContent = '0' + minsLeft + ':00';
+      }
+      else {
+        timeLeft.textContent = minsLeft + ':00';
+      }
+    }
   }
 }
 
-sessionDecrement.addEventListener('click', subtractSession);
-sessionIncrement.addEventListener('click', addSession);
-
-
-
-const timeLeft = document.getElementById('time-left');
-let minsLeft = Number(timeLeft.textContent.match(/\d+/));
-let secsLeft = Number(timeLeft.textContent.match(/(?<=:)\d+/));
-
-console.log(minsLeft);
-console.log(secsLeft);
-
-function subtract1() {
+function runTimer() {
   secsLeft--;
-  //if minsLeft and secsLeft == 0 (do stuff)
-  if (secsLeft == -1) {
+  if (minsLeft == 0 && secsLeft == -1) {
+    console.log('swapped');
+    swapBreak();
+  }
+  else if (secsLeft == -1) {
     minsLeft--;
     secsLeft = 59;
-    timeLeft.textContent = minsLeft + ':' + secsLeft;
+    if (minsLeft < 10) {
+      timeLeft.textContent = '0' + minsLeft + ':' + secsLeft;
+    }
+    else {
+      timeLeft.textContent = minsLeft + ':' + secsLeft;
+    }
   }
   else if (secsLeft < 10) {
-    timeLeft.textContent = minsLeft + ':0' + secsLeft;
+    if (minsLeft < 10) {
+      timeLeft.textContent = '0' + minsLeft + ':0' + secsLeft;
+    }
+    else {
+      timeLeft.textContent = minsLeft + ':0' + secsLeft;
+    }
   }
   else {
-    timeLeft.textContent = minsLeft + ':' + secsLeft;
+    if (minsLeft < 10) {
+      timeLeft.textContent = '0' + minsLeft + ':' + secsLeft;
+    }
+    else {
+      timeLeft.textContent = minsLeft + ':' + secsLeft;
+    }
   }
 }
 
-let intervalID;// = setInterval(subtract1, 100);
-
-const startStop = document.getElementById('start_stop');
-
-let paused = true;
-
 function toggleTimer() {
-  console.log(paused);
   if (paused == true) {
     paused = false;
-    console.log(paused);
-    intervalID = setInterval(subtract1, 100);
+    intervalID = setInterval(runTimer, 50);
   }
   else {
     paused = true;
     clearInterval(intervalID);
   }
 }
-
-
-startStop.addEventListener('click', toggleTimer);
-
-const reset = document.getElementById('reset');
 
 function resetTimer() {
   if (paused == false) {
     paused = true;
     clearInterval(intervalID);
   }
-  minsLeft = 25;
+  timerLabel.textContent = 'Session';
+  minsLeft = 1;
   secsLeft = 0;
-  timeLeft.textContent = minsLeft + ':00';
-  breakLengthVal = 5;
+  breakLengthVal = 2;
   breakLength.textContent = breakLengthVal;
-  sessionLengthVal = 25;
+  sessionLengthVal = 1;
   sessionLength.textContent = sessionLengthVal;
+  if (minsLeft < 10) {
+    timeLeft.textContent = '0' + minsLeft + ':00';
+  }
+  else {
+    timeLeft.textContent = minsLeft + ':00';
+  }
+  beep.pause();
+  beep.currentTime = 0;
 }
 
-reset.addEventListener('click', resetTimer);
+function swapBreak() {
+  beep.play();
+  if (timerLabel.textContent == 'Session') {
+    console.log('timer-label set to Break');
+    timerLabel.textContent = 'Break';
+    minsLeft = breakLengthVal;
+  }
+  else {
+    console.log('timer-label set to Session');
+    timerLabel.textContent = 'Session';
+    minsLeft = sessionLengthVal;
+  }
+  secsLeft = 0;
+  if (minsLeft < 10) {
+    timeLeft.textContent = '0' + minsLeft + ':00';
+  }
+  else {
+    timeLeft.textContent = minsLeft + ':00';
+  }
+}
 
 
-//CREATE REPOSITORY AND CONTINUE USER STORIES
+//REFACTOR CODE AND ADD STYLING
